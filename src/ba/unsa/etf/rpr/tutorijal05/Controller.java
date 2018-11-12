d15279f0643fe18d81b07730b09dd41794bb806c
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-11-12 09:25:40 +0100

    Dodane sve tipke u kod

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,70 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Controller {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 
+public class Controller {
+    private SimpleStringProperty ulaz;
+    public Controller() {
+        ulaz = new SimpleStringProperty("0");
+    }
+
+    public String getUlaz() {
+        return ulaz.get();
+    }
+
+    public SimpleStringProperty ulazProperty() {
+        return ulaz;
+    }
+
+    public void nula(ActionEvent actionEvent) {
+    }
+
+    public void zarez(ActionEvent actionEvent) {
+    }
+
+    public void jedan(ActionEvent actionEvent) {
+    }
+
+    public void dva(ActionEvent actionEvent) {
+    }
+
+    public void tri(ActionEvent actionEvent) {
+    }
+
+    public void cetiri(ActionEvent actionEvent) {
+    }
+
+    public void pet(ActionEvent actionEvent) {
+    }
+
+    public void sest(ActionEvent actionEvent) {
+    }
+
+    public void plus(ActionEvent actionEvent) {
+    }
+
+    public void sedam(ActionEvent actionEvent) {
+    }
+
+    public void devet(ActionEvent actionEvent) {
+    }
+
+    public void osam(ActionEvent actionEvent) {
+    }
+
+    public void postotak(ActionEvent actionEvent) {
+    }
+
+    public void podijeljeno(ActionEvent actionEvent) {
+    }
+
+    public void put(ActionEvent actionEvent) {
+    }
+
+    public void minus(ActionEvent actionEvent) {
+    }
+
+    public void jednako(ActionEvent actionEvent) {
+    }
 }
